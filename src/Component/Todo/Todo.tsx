102245736a8e7ec8dd938228402fb6eb488a1836
Prince kaushal <prince.kaushal@appknit.io>
2021-04-02 11:38:34 +0530

    added validation to todo component

--- src/Component/Todo/Todo.tsx
+++ src/Component/Todo/Todo.tsx
@@ -10,14 +10,36 @@ export const Todo:React.FC<IProps>=(props:IProps)=>{
     console.log('params data',props.match.params.name)
     const [todo,setTodo]=useState<string>('')
     const [todoArray,setTodoArray]=useState<Array<string>>([])
-
+    const [todoError,setTodoError]=useState<string>('')
+    // on change of input
     const handleTodo=(e:React.ChangeEvent<HTMLInputElement>)=>{
         setTodo(e.target.value)
+        setTodoError('')
     }
+    // validation
+    const validation=()=>{
+        let todoError=''
+        if(!todo){
+            todoError='todos cannot be empty'
+        }
+        else if(todo.length<9){
+            todoError='todos can be of minimum 10 character long'
+        }
+        if(todoError){
+            setTodoError(todoError)
+            return false  
+        }
+        return true
+    }
+    // add todo to the array on click of add button
     const addTodoArray=()=>{
-        setTodoArray([...todoArray,todo])
-        setTodo('')
+        const isValid=validation()
+        if(isValid){
+            setTodoArray([...todoArray,todo])
+            setTodo('')
+        }
     }
+    // on double click of list remove todo
     const deleteTodo=(id:number)=>{
         const deleteTodo=todoArray.filter((ele?:string,i?:number)=>id!==i)
         setTodoArray(deleteTodo)
@@ -28,12 +50,13 @@ export const Todo:React.FC<IProps>=(props:IProps)=>{
                 <h2>Hello {props.match.params.name}</h2>
             </div>
             <div>
-                <table>
-                    <tr>
-                        <th><input type='text' value={todo} onChange={handleTodo}/></th>
-                        <td><button onClick={addTodoArray}>Add</button></td>
-                    </tr>
-                </table>
+                <div>
+                    <input type='text' value={todo} onChange={handleTodo}/>
+                    <div>{todoError}</div>
+                </div>
+                <div>
+                    <button onClick={addTodoArray}>Add</button>
+                </div>
             </div>
             <div>
                 <ul>
